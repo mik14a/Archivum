029fde1849474c98b7259a06143fc524d5c0bf79
mik14a <mik14a@gmail.com>
2025-01-19 11:10:35 +0900

    Hide system navigation bar on Android 10 and later

--- Archivum.Maui/Platforms/Android/MainActivity.cs
+++ Archivum.Maui/Platforms/Android/MainActivity.cs
@@ -1,5 +1,9 @@
+using System;
 using Android.App;
 using Android.Content.PM;
+using Android.OS;
+using Android.Views;
+using AndroidX.Core.View;
 using Microsoft.Maui;
 
 namespace Archivum.Platforms.Android;
@@ -7,4 +11,15 @@ namespace Archivum.Platforms.Android;
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override void OnCreate(Bundle? savedInstanceState) {
+        base.OnCreate(savedInstanceState);
+
+        // Android 11 or later
+        if (Window != null && OperatingSystem.IsAndroidVersionAtLeast(30)) {
+            Window.InsetsController?.Hide(WindowInsets.Type.NavigationBars());
+            if (Window.InsetsController != null) {
+                Window.InsetsController.SystemBarsBehavior = WindowInsetsControllerCompat.BehaviorShowTransientBarsBySwipe;
+            }
+        }
+    }
 }
